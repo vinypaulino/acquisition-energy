f1eab132a708efd96fc654dcb7aff77aea84467b
vinypaulino <vinydos3@gmail.com>
2023-02-09 14:47:13 -0300

    test: creates consumption tests

---
+++ src/domain/consumption-classes.spec.ts
@@ -0,0 +1,15 @@
+import ConsumptionClasses from './consumption-classes';
+
+describe('consumptionClassesTests', () => {
+    it('shouldReturnEmptyStringWhenIsElegible', () => {
+        const verifierConsumptionClasses = new ConsumptionClasses();
+        expect(verifierConsumptionClasses.verifyEligibility('Comercial')).toBe('');
+    });
+
+    it('should return "Classe de consumo não aceita" when is not elegible', () => {
+        const verifierConsumptionClasses = new ConsumptionClasses();
+        expect(verifierConsumptionClasses.verifyEligibility('Rural')).toBe(
+            'Classe de consumo não aceita'
+        );
+    });
+});
