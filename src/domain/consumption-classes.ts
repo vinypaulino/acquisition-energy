b2ec121044f45e173f9d99462c544b662fd1758b
vinypaulino <vinydos3@gmail.com>
2023-02-13 19:58:57 -0300

    refactor: update consumption classes

--- src/domain/consumption-classes.ts
+++ src/domain/consumption-classes.ts
@@ -1,16 +1,12 @@
 export default class ConsumptionClasses {
-    private eligibleClasses: string[];
-
-    constructor() {
-        this.eligibleClasses = ['Comercial', 'Residencial', 'Industrial']
+  private eligibleClasses: string[];
+  constructor() {
+    this.eligibleClasses = ['comercial', 'residencial', 'industrial'];
+  }
+  verifyEligibility(clientClass: string): string {
+    if (this.eligibleClasses.includes(clientClass.toLowerCase())) {
+      return '';
     }
-
-    verifyEligibility(clientClass: string): string {
-        if (this.eligibleClasses.includes(clientClass)) {
-            return '';
-        }
-
-        return 'Classe de consumo não aceita';
-    }
-
+    return 'Classe de consumo não aceita';
+  }
 }
