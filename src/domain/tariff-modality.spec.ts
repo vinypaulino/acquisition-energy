ba614d030ad4a9b09cf5ff0861726a485571e5da
vinypaulino <vinydos3@gmail.com>
2023-02-13 19:54:44 -0300

    test: creates tariff modality tests

---
+++ src/domain/tariff-modality.spec.ts
@@ -0,0 +1,15 @@
+import TariffModality from './tariff-modality';
+
+describe('tariffModalityTests', () => {
+  it('shouldReturnEmptyStringWhenIsEligible', () => {
+    const verifierTariffModality = new TariffModality();
+    expect(verifierTariffModality.verifyEligibility('Branca')).toBe('');
+  });
+
+  it('should return "Classe de consumo não aceita" when is not eligible', () => {
+    const verifierTariffModality = new TariffModality();
+    expect(verifierTariffModality.verifyEligibility('Verde')).toBe(
+      'Modalidade tarifária não aceita',
+    );
+  });
+});
