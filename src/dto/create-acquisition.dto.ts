3446c438e052d2dd20de54de05323dbf9903dffd
Vini Paulino <vinydos3@gmail.com>
2023-02-26 22:02:56 -0300

    feat: adds validation to input dto

--- src/dto/create-acquisition.dto.ts
+++ src/dto/create-acquisition.dto.ts
@@ -1,7 +1,39 @@
+import {
+  ArrayMaxSize,
+  ArrayMinSize,
+  IsIn,
+  IsNotEmpty,
+  Max,
+  MaxLength,
+  Min,
+  MinLength,
+} from 'class-validator';
+
 export class CreateAcquisitionDto {
+  @MinLength(11)
+  @MaxLength(14)
+  @IsNotEmpty()
   numeroDoDocumento: string;
+
+  @IsIn(['monofasico', 'bifasico', 'trifasico'])
+  @IsNotEmpty()
   tipoDeConexao: string;
+
+  @IsIn(['residencial', 'industrial', 'comercial', 'rural', 'poderPublico'])
+  @IsNotEmpty()
   classeDeConsumo: string;
+
+  @IsIn(['azul', 'branca', 'verde', 'convencional'])
+  @IsNotEmpty()
   modalidadeTarifaria: string;
+
+  @ArrayMinSize(3)
+  @ArrayMaxSize(12)
+  @Min(0, {
+    each: true,
+  })
+  @Max(9999, {
+    each: true,
+  })
   historicoDeConsumo: number[];
 }
